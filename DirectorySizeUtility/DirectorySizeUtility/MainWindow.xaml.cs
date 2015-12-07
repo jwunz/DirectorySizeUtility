ae5f94f84526f55d7b0b3972ca8777e31dc283dc
Thomas Feuerborn <Thomas Feuerborn>
2015-12-07 16:48:16 -0700

    made a VERY rough working copy that can be turned in as it satisfies the first two requirements, sorting still unimplemented
    Signed-off-by: Thomas Feuerborn <Thomas Feuerborn>

--- DirectorySizeUtility/DirectorySizeUtility/MainWindow.xaml.cs
+++ DirectorySizeUtility/DirectorySizeUtility/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,156 @@ namespace DirectorySizeUtility
     /// </summary>
     public partial class MainWindow : Window
     {
+        public long ROOT_SIZE;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileExplorer.Items.Clear();
+
+            DirectoryInfo item = new DirectoryInfo(SearchBox.Text);
+            TreeViewItem root = new TreeViewItem();
+
+            this.ROOT_SIZE = DirSize(item);
+
+            root.Header = String.Format("{0}Kb -- {1} : {2}", ROOT_SIZE / 1000, item.Name, PercentageOfParent(ROOT_SIZE));
+            FileExplorer.Items.Add(root);
+
+            Dig(item, root);
+
+        }
+
+        public String PercentageOfParent(long Size)
+        {
+            string outstring = "";
+            int percentage = (int) Math.Floor(100 * ((double)Size / ROOT_SIZE));
+
+            outstring = percentage.ToString() + "% : " + new String('▓', percentage) + new string('░', 100 - percentage);
+            //outstring = percentage.ToString() + "% : " + new string('░', 100 - percentage) + new String('▓', percentage);
+            return outstring;
+
+            //return percentage.ToString();
+        }
+
+        public String PercentageOfParent(long ParentSize, long Size)
+        {
+            string outstring = "";
+            int percentage = (int)Math.Floor(100 * ((double)Size / ParentSize));
+
+            outstring = percentage.ToString() + "% : " + new String('█', percentage);
+            return outstring;
+
+            //return percentage.ToString();
+        }
+
+        private long DirSize(DirectoryInfo item)
+        {
+            long Size = 0;
+
+            try
+            {
+                FileInfo[] fis = item.GetFiles();
+                foreach (FileInfo fi in fis)
+                {
+                    Size += fi.Length;
+                }
+                DirectoryInfo[] dis = item.GetDirectories();
+                foreach (DirectoryInfo di in dis)
+                {
+                    Size += DirSize(di);
+                }
+            }
+            catch ( Exception e )
+            {
+                //do nothing
+            }
+
+            return (Size);
+        }
+
+        private void Dig(DirectoryInfo dir, TreeViewItem t)
+        {
+            try
+            {
+                foreach (DirectoryInfo item in dir.EnumerateDirectories())
+                {
+                    var subitem = new TreeViewItem();
+
+                    //subitem.Header = item.Name;
+
+                    long itemsize = DirSize(item);
+
+                    subitem.Header = String.Format("{0}Kb -- {1} : {2}", (itemsize / 1000), item.Name, PercentageOfParent(itemsize));
+                    //subitem.Header = String.Format("{0}Kb -- {1}", (itemsize / 1000), item.Name);
+
+                    t.Items.Add(subitem);
+
+                    Dig(item, subitem);
+                }
+                foreach (FileInfo item in dir.EnumerateFiles()) {
+                    var subitem = new TreeViewItem();
+                    subitem.Header = String.Format("{0}Kb -- {1} : {2}", (item.Length / 1000), item.Name, PercentageOfParent(item.Length));
+
+                    t.Items.Add(subitem);
+                }
+            }
+            catch (Exception e)
+            {
+                var subitem = new TreeViewItem() { Header = "YOU GET NOTHING" };
+
+                t.Items.Add(subitem);
+
+                Console.WriteLine("YOU GET NOTHING");
+            }
+        }
+
+
     }
+
+    public class HeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int hold;
+            bool test = Int32.TryParse(value.ToString(), out hold);
+            if (test)
+            {
+                return hold - 20;
+            }
+
+            return Int32.MaxValue;
+            //throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HeightConverter2 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int hold;
+            bool test = Int32.TryParse(value.ToString(), out hold);
+            if (test)
+            {
+                return hold - 40;
+            }
+
+            return Int32.MaxValue;
+            //throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
